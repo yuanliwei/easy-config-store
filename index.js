58d5a4c8c155e5e7b4a7530c6636f74721b65b6d
yuanliwei <891789592@qq.com>
2018-08-28 00:14:25 +0800

    add readme

--- index.js
+++ index.js
@@ -1,103 +1,102 @@
-// var config = {};
-var defaultCfg;
-var cfgFilePath;
-var rawConfig = {}
+let defaultCfg;
+let saveConfigFunc;
+let rawConfig = {}
+let config
+let timer
 
-var handle = {
-  get: function (oTarget, sKey) {
-    if (sKey == 'toJSON') {
-      for (var k in oTarget) {
-        if (oTarget.hasOwnProperty(k)) {
-          var v = oTarget[k]
-          if (typeof(v) == 'object' && Object.keys(v).length==0) {
-            oTarget[k] = ''.undefined
-          }
-        }
-      }
-      return JSON.stringify(oTarget)
-    }
-    if (sKey == 'valueOf') {
-      return oTarget.valueOf()
-    }
-    var result = oTarget[sKey]
-    if (typeof result == 'undefined') {
+let handle = {
+  get: function(oTarget, sKey) {
+    let result = oTarget[sKey]
+    if (typeof result == 'undefined' && sKey != 'toJSON') {
       result = oTarget[sKey] = new Proxy({}, handle)
     }
     return result
   },
-  set: function (oTarget, sKey, vValue) {
+  set: function(oTarget, sKey, vValue) {
     if (vValue && typeof vValue == 'object') {
       vValue = new Proxy(vValue, handle)
     }
     oTarget[sKey] = vValue
+    saveConfigFunc(config)
     return true;
   }
 }
 
+config = new Proxy(rawConfig, handle)
+
 Object.defineProperties(rawConfig, {
-  setDefaultConfig:{
-    value: (cfg)=>{
-      defaultCfg = cfg
-      console.log('call setDefaultConfig', cfg);
+  cfgClear: {
+    value: () => {
+      for (let k in config) {
+        delete config[k]
+      }
     }
   },
-  setConfigPath:{
-    value: (cfgPath)=>{
-      cfgFilePath = cfgPath
-      console.log('call setConfigPath', cfgPath);
+  cfgReset: {
+    value: () => {
+      for (let k in config) {
+        delete config[k]
+      }
+      cfg = Object.assign({}, defaultCfg)
+      for (let k in cfg) {
+        config[k] = cfg[k]
+      }
+    }
+  },
+  setOptions: {
+    value: (cfg, onSaveCallback) => {
+      if (onSaveCallback) {
+        saveConfigFunc = onSaveCallback
+      }
+      defaultCfg = Object.assign({}, cfg)
+      for (let k in cfg) {
+        config[k] = cfg[k]
+      }
+    }
+  },
+  cfgUseFile: {
+    value: (cfgPath) => {
+      const fs = require('fs');
+      if (!fs.existsSync(cfgPath)) {
+        fs.writeFileSync(cfgPath, '{}', 'utf-8')
+      }
+      let cfg = JSON.parse(fs.readFileSync(cfgPath, 'utf-8'))
+      config.setOptions(cfg, (cfg) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          fs.writeFileSync(cfgPath, JSON.stringify(cfg), 'utf-8')
+        }, 300)
+      })
+    }
+  },
+  cfgUseLocalStorage: {
+    value: (key) => {
+      let cfg = JSON.parse(localStorage[key])
+      config.setOptions(cfg, (cfg) => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          localStorage[key] = JSON.stringify(cfg)
+        }, 300)
+      })
+    }
+  },
+  cfgUseMemory: {
+    value: (key) => {
+      config.setOptions({}, (cfg) => {})
     }
   }
 })
-var config = new Proxy(rawConfig, handle)
 
 function build() {
-  // var json = getJson()
-  // buildConfig(json)
-}
-
-function buildConfig(obj) {
-  var cfgs = {}
-  for (var k in obj) {
-    if (obj.hasOwnProperty(k)) {
-      cfgs[k] = {
-        value: obj[k],
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        get: function () {
-          console.log('get:', this.value, 'this:',this);
-          this.value
-        },
-        set: function (value) {
-          console.log('set:',value, 'this:',this);
-          this.value = value
-          if (typeof value == 'object') {
-            buildConfig(value)
-          }
-        }
-      }
-    }
+  if (typeof(localStorage) == 'object') {
+    config.cfgUseLocalStorage('easy-config-store')
+  } else if (typeof(require('os')) == 'object') {
+    const os = require('os');
+    const path = require('path');
+    config.cfgUseFile(path.join(os.tmpdir(), 'easy-config-store.cfg'))
+  } else {
+    config.cfgUseMemory()
   }
-  Object.defineProperties(config, cfgs)
-}
-
-function save() {
-
-}
-
-function getJson() {
-  var cfg;
-  try {
-    if (typeof localStorage != 'undefined') {
-      cfg = JSON.parse(localStorage['easy-config-store'])
-    } else {
-      cfg = JSON.parse(require('fs').readFileSync(cfgFilePath, 'utf-8'))
-    }
-  } catch (e) { console.warn(e); }
-  if (typeof cfg != 'object') {
-    cfg = defaultCfg || {}
-  }
-  return cfg
 }
 
 build()
--- test.js
+++ test.js
@@ -1,6 +1,44 @@
 const config = require('./index');
 const path = require('path');
 
+config.cfgClear()
+config.cfgUseMemory()
+config.user.name = 'Y'
+config.birth.day = 26
+config.birth.month = 8
+config.books = []
+config.books[0].name = "Thinking in JavaScript"
+console.log(JSON.stringify(config))
+return
+
+console.log(JSON.stringify(config));
+// << {"name":"yuanliwei","age":13}
+console.log('测试 cfgClear()');
+// << 测试 cfgClear()
+config.name = 'yuanliwei'
+console.log(JSON.stringify(config.AGE));
+// << {"name":"yuanliwei","age":13,"AGE":{}}
+console.log(JSON.stringify(config));
+// <<
+config.cfgClear()
+console.log(JSON.stringify(config));
+console.log('Object.keys(config) == 0 :', Object.keys(config) == 0);
+
+console.log('\n测试 cfgDefault({})');
+config.setOptions({
+  name: 'yuanliwei',
+  age: 13
+})
+console.log(JSON.stringify(config));
+
+console.log('\n测试 cfgReset()');
+config.books = []
+config.books[0] = {name:'世界尽头', price: 34.8}
+config.books[0].car.oldPrice = 7
+console.log(JSON.stringify(config));
+config.cfgReset()
+console.log(JSON.stringify(config));
+
 console.log(config.name1);
 console.log(JSON.stringify(config.name1));
 config.name.leng = 'yuanliwei'
@@ -11,19 +49,22 @@ console.log(JSON.stringify(config));
 config.obj={a:2,b:3,c:{w:3,e:'t'}}
 console.log(JSON.stringify(config));
 console.log('config.obj.c.e:',config.obj.c.e);
-return
 
-config.setDefaultConfig({
+config.setOptions({
   name: 'yuanliwei'
 })
-config.setConfigPath(path.join(__dirname, '.git/cfg.json'))
+config.cfgUseFile(path.join(__dirname, '.git/cfg.json'))
+
+config.cfgReset()
+config.cfgClear()
 
 console.log('config.name', config.name);
 config.age = '13'
 console.log('config.age', config.age);
 // console.log('config', config);
-config.books.book1.name = 'stars'
-config.books.book1.price = 45
+config.books = []
+config.books[0].name = 'stars'
+config.books[0].price = 45
 
 // console.log('config', config);
 console.log('---------------');
@@ -31,6 +72,6 @@ console.log('keys:', Object.keys(config));
 console.log('---------------');
 console.log('json:', JSON.stringify(config));
 
-config.name.bind(document.getElementById('id'))
-config.name.bind($('input'))
-config.name.bind($('input'), true)
+// config.name.bind(document.getElementById('id'))
+// config.name.bind($('input'))
+// config.name.bind($('input'), true)
